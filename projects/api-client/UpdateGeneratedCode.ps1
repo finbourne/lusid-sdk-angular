e5173ec3708d335de26336ffe8c9bdebc25b4604
Will Stewart <william.stewart@finbourne.com>
2018-07-25 15:01:57 +0100

    Make changes for angular 6

--- projects/api-client/UpdateGeneratedCode.ps1
+++ projects/api-client/UpdateGeneratedCode.ps1
@@ -2,13 +2,19 @@ $baseDir = $PSScriptRoot
 $genRelDir = "src/lib/.generated"
 $genFullDir = Join-Path -Path $baseDir -ChildPath $genRelDir
 
-Get-ChildItem -Path $genFullDir -Include * | Remove-Item -Recurse
+# Get-ChildItem -Path $genFullDir -Include * | Remove-Item -Recurse
 
-# Generate the required files
+# # Generate the required files
 docker run --rm -v ${baseDir}:/local `
     swaggerapi/swagger-codegen-cli generate `
     -i https://api.finbourne.com/swagger/v0/swagger.json `
     -l typescript-angular `
     -o /local/$genRelDir
 
+Start-Sleep -Seconds 5
+
 # Modify the generated code for angular 6
+$tsFiles = Get-ChildItem -Path $genFullDir -Filter *.ts -Recurse
+foreach ($file in $tsFiles) {
+    ( Get-Content $file.FullName) | ForEach-Object { $_ -replace "from 'rxjs/Observable';", "from 'rxjs';" } | Set-Content $file.FullName
+}
--- projects/api-client/src/public_api.ts
+++ projects/api-client/src/public_api.ts
@@ -5,3 +5,4 @@
 export * from './lib/api-client.service';
 export * from './lib/api-client.component';
 export * from './lib/api-client.module';
+export * from './lib/.generated';
