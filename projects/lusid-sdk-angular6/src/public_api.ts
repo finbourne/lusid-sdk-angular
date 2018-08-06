c446c8b86af679f06b35d87551d55969edb7db5e
Bartosz Wasiluk <b.wasiluk@codeandpepper.com>
2018-08-06 15:53:23 +0200

    Fixes metadata generation

--- projects/lusid-sdk-angular6/src/public_api.ts
+++ projects/lusid-sdk-angular6/src/public_api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of lusid-sdk-angular6
  */
 
-export * from './lib/lusid-sdk-angular6.service';
-export * from './lib/lusid-sdk-angular6.module';
-export * from './lib/generated';
+export * from './lib/generated/index';
