e5960a5cc70032186fc4b5d807e74e91a960d327
Will Stewart <william.stewart@finbourne.com>
2018-09-13 11:41:49 +0100

    Update karma (from 2.0.5 to 3.0.0) Fix some broken tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'lusid'`, async(() => {
+  it(`should have as title 'Finbourne LUSID angular SDK'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('lusid');
+    expect(app.title).toEqual('Finbourne LUSID angular SDK');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to lusid-sdk-angular!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Finbourne LUSID angular SDK!');
   }));
 });
