29a6e04fb3a7de76e9957b5934bb9b0c96bcfbdf
Will Stewart <william.stewart@finbourne.com>
2019-08-29 13:37:26 +0100

    WEB-1036 : Upgrade to support angular 8

--- generate.sh
+++ generate.sh
@@ -7,7 +7,7 @@ if [[ ${#1} -eq 0 ]]; then
 fi
 
 gen_root=/usr/src
-sdk_output_folder=$gen_root/projects/lusid-sdk-angular7/src/lib/generated
+sdk_output_folder=$gen_root/projects/lusid-sdk-angular8/src/lib/generated
 swagger_file=$gen_root/$1
 
 #   remove all previously generated files
@@ -23,7 +23,7 @@ java -jar /usr/swaggerjar/swagger-codegen-cli.jar generate \
     -l typescript-angular \
     -o $sdk_output_folder \
     -c $gen_root/config.json \
-    --additional-properties ngVersion=7
+    --additional-properties ngVersion=8
 
 find $sdk_output_folder -type f | xargs -I £ sed -i 's/rxjs\/Observable/rxjs/g' £
 
@@ -33,11 +33,11 @@ sdk_version=$(cat $swagger_file | jq -r '.info.version')
 echo $sdk_version
 
 echo "updating version in package.json to '$sdk_version'"
-package_json=$gen_root/projects/lusid-sdk-angular7/package.json
+package_json=$gen_root/projects/lusid-sdk-angular8/package.json
 cat $package_json | jq -r --arg SDK_VERSION "$sdk_version" '.version |= $SDK_VERSION' > temp && mv temp $package_json
 
 echo "installing packages"
 npm ci
 
 echo "running ng build"
-ng build lusid-sdk-angular7
+ng build lusid-sdk-angular8
