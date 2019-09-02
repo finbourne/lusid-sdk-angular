377306108f88cfdf1e578fe4e6556f45cd576d05
Will Stewart <william.stewart@finbourne.com>
2019-09-02 17:10:43 +0100

    WEB-1036 : Update to angular 8

--- generate.sh
+++ generate.sh
@@ -6,24 +6,28 @@ if [[ ${#1} -eq 0 ]]; then
     exit 1
 fi
 
-gen_root=/usr/src
-sdk_output_folder=$gen_root/projects/lusid-sdk-angular7/src/lib/generated
-swagger_file=$gen_root/$1
+angular_version=8;
 
-#   remove all previously generated files
+gen_root=/usr/src
+sdk_output_folder=$gen_root/projects/lusid-sdk-angular$angular_version/src/lib/generated
+swagger_file=$gen_root/$1
+echo "swagger file $gen_root/$1"
+echo "sdk output folder: $angusdk_output_folderlarVersion"
+
+# remove all previously generated files
 shopt -s extglob
 echo "removing previous sdk:"
 rm -rf $sdk_output_folder/
 shopt -u extglob
 
-echo "generating sdk"
+echo "generating the LUSID API sdk"
 
 java -jar /usr/swaggerjar/swagger-codegen-cli.jar generate \
     -i $swagger_file \
     -l typescript-angular \
     -o $sdk_output_folder \
     -c $gen_root/config.json \
-    --additional-properties ngVersion=7
+    --additional-properties ngVersion=$angular_version
 
 find $sdk_output_folder -type f | xargs -I £ sed -i 's/rxjs\/Observable/rxjs/g' £
 
@@ -33,11 +37,11 @@ sdk_version=$(cat $swagger_file | jq -r '.info.version')
 echo $sdk_version
 
 echo "updating version in package.json to '$sdk_version'"
-package_json=$gen_root/projects/lusid-sdk-angular7/package.json
+package_json=$gen_root/projects/lusid-sdk-angular$angular_version/package.json
 cat $package_json | jq -r --arg SDK_VERSION "$sdk_version" '.version |= $SDK_VERSION' > temp && mv temp $package_json
 
 echo "installing packages"
 npm ci
 
 echo "running ng build"
-ng build lusid-sdk-angular7
+ng build lusid-sdk-angular$angular_version
